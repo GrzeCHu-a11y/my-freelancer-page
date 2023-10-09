b6d57a1a6f27f929fee901a8f97990c76acaccb6
GrzeCHu-a11y <ggonczowski@zsp5kozuchow.pl>
2023-10-09 23:28:44 +0200

    nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,33 @@
-import {createBrowserRouter, createRoutesFromElements, Route, Outlet, RouterProvider} from "react-router-dom";
-import './App.css'
+import { createBrowserRouter, createRoutesFromElements, Route, Outlet, RouterProvider } from "react-router-dom";
+import "./App.css";
 import Navbar from "./components/organisms/Navbar/Navbar.jsx";
 import Home from "./pages/Home.jsx";
 import About from "./pages/About.jsx";
 function App() {
-
-    const router = createBrowserRouter(
-        createRoutesFromElements(
-            <Route path="/" element={<Root/>}>
-                <Route index element={<Home/>}/>
-                <Route path="/about" element={<About/>}/>
-            </Route>
-        )
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<Root />}>
+        <Route index element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Route>
     )
+  );
 
   return (
-   <div className="App">
-       <RouterProvider router={router}/>
-   </div>
-  )
+    <div className="App">
+      <RouterProvider router={router} />
+    </div>
+  );
 }
 
 const Root = () => {
-    return (
+  return (
     <>
-        <Navbar/>
-        <main>
-            <Outlet/>
-        </main>
+      <Navbar />
+      <main>
+        <Outlet />
+      </main>
     </>
-    )
-}
-export default App
+  );
+};
+export default App;
--- src/components/organisms/Navbar/Navbar.jsx
+++ src/components/organisms/Navbar/Navbar.jsx
@@ -1,18 +1,29 @@
 import { NavLink } from "react-router-dom";
+import { useState } from "react";
 import style from "./Navbar.module.scss";
 
 const Navbar = () => {
+  const [toogleMenu, setTooglemenu] = useState(false);
+
+  const toogleNav = () => {
+    setTooglemenu(!toogleMenu);
+  };
+
   return (
     <nav className={style.nav}>
-      <header>
+      <header className={style.header}>
         <h3>G.G Dev</h3>
       </header>
-      <NavLink className={style.navlink} to="/">
-        Home
-      </NavLink>
-      <NavLink className={style.navlink} to="/about">
-        About
-      </NavLink>
+      <div className={style.hamCon}></div>
+      <img className={style.hamburger} onClick={toogleNav} src="src/assets/hamburger.png" alt="" />
+      <div className={`${toogleMenu ? style.itemsContainerOpen : style.itemsContainerClose}`}>
+        <NavLink className={style.navlink} to="/">
+          Home
+        </NavLink>
+        <NavLink className={style.navlink} to="/about">
+          About
+        </NavLink>
+      </div>
     </nav>
   );
 };
