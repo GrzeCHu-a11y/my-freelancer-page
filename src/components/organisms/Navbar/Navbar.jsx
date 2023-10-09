d0ab7bec0b4751b5f0241bff58e4dfaf3f78a9c5
GrzeCHu-a11y <ggonczowski@zsp5kozuchow.pl>
2023-10-09 17:46:38 +0200

    basic setup

--- src/components/organisms/Navbar/Navbar.jsx
+++ src/components/organisms/Navbar/Navbar.jsx
@@ -1,12 +1,20 @@
-import {NavLink} from "react-router-dom";
-import style from "./Navbar.module.scss"
+import { NavLink } from "react-router-dom";
+import style from "./Navbar.module.scss";
+
 const Navbar = () => {
-    return (
-        <nav className={style.nav}>
-            <NavLink to="/">Home</NavLink>
-            <NavLink to="/about">About</NavLink>
-        </nav>
-    );
+  return (
+    <nav className={style.nav}>
+      <header>
+        <h3>G.G Dev</h3>
+      </header>
+      <NavLink className={style.navlink} to="/">
+        Home
+      </NavLink>
+      <NavLink className={style.navlink} to="/about">
+        About
+      </NavLink>
+    </nav>
+  );
 };
 
 export default Navbar;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,16 @@
+import style from "./Home.module.scss";
 
 const Home = () => {
-    return (
-        <div>
-            <h1>Home</h1>
-        </div>
-    );
+  return (
+    <div>
+      <h1>Profesjonalne strony internetowe Grzegorz Gończowski</h1>
+      <br />
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eligendi quisquam iusto suscipit harum soluta sapiente quae recusandae impedit
+        optio temporibus minus numquam debitis eaque dolores hic, fuga voluptatem magni ipsa!
+      </p>
+    </div>
+  );
 };
 
 export default Home;
